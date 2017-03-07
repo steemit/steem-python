bb44f4c8efacb37e856c8b7d4f388b8e46fd223c
furion <_@furion.me>
2017-03-07 13:37:36 +0100

    [steem] full steemd API compatibility, new spec format

--- steempy/steemd.py
+++ steempy/steemd.py
@@ -1,110 +1,500 @@
 import sys
 from functools import partial
 
-api_methods = {
-    "api": {
-        "cancel_all_subscriptions": 0,
-        "get_account_bandwidth": [('account', 'str'), ('bandwidth_type', 'object')],
-        "get_account_count": [],
-        "get_account_history": [('account', 'str'), ('index_from', 'int'), ('limit', 'int')],
-        "get_account_references": [('account_name', 'str')],
-        "get_account_votes": [('account', 'str')],
-        "get_accounts": [('account_names', 'list')],
-        "get_active_categories": [('after', 'str'), ('limit', 'int')],  # deprecated?
-        "get_active_votes": [('author', 'str'), ('permlink', 'str')],
-        "get_active_witnesses": [],
-        "get_best_categories": [('after', 'str'), ('limit', 'int')],  # deprecated?
-        "get_block": [('block_num', 'int')],
-        "get_block_header": [('block_num', 'int')],
-        "get_chain_properties": [],
-        "get_config": [],
-        "get_content": [('author', 'str'), ('permlink', 'str')],
-        "get_content_replies": [('author', 'str'), ('permlink', 'str')],
-        "get_conversion_requests": [('account', 'str')],
-        "get_current_median_history_price": [],
-        "get_discussions_by_active": [('discussion_query', 'dict')],
-        "get_discussions_by_author_before_date": [('author', 'str'), ('start_permlink', 'str'), ('before_date', 'object'), ('limit', 'int')],
-        "get_discussions_by_blog": [('discussion_query', 'dict')],
-        "get_discussions_by_cashout": [('discussion_query', 'dict')],
-        "get_discussions_by_children": [('discussion_query', 'dict')],
-        "get_discussions_by_comments": [('discussion_query', 'dict')],
-        "get_discussions_by_created": [('discussion_query', 'dict')],
-        "get_discussions_by_feed": [('discussion_query', 'dict')],
-        "get_discussions_by_hot": [('discussion_query', 'dict')],
-        "get_discussions_by_payout": [('discussion_query', 'dict')],
-        "get_discussions_by_promoted": [('discussion_query', 'dict')],
-        "get_discussions_by_trending": [('discussion_query', 'dict')],
-        "get_discussions_by_trending30": [('discussion_query', 'dict')],
-        "get_discussions_by_votes": [('discussion_query', 'dict')],
-        "get_dynamic_global_properties": [],
-        "get_escrow": [('from_account', 'str'), ('escrow_id', 'int')],
-        "get_expiring_vesting_delegations": [('author', 'str'), ('from_time', 'object'), ('limit', 'int')],  # ?
-        "get_feed_history": [],
-        "get_hardfork_version": [],
-        "get_key_references": 0,  # deprecated, use account_by_key_api::get_key_references
-        "get_liquidity_queue": [('start_account', 'str'), ('limit', 'int')],
-        "get_miner_queue": [],
-        "get_next_scheduled_hardfork": [],
-        "get_open_orders": [('account', 'str')],
-        "get_ops_in_block": [('block_num', 'int'), ('virtual_only', 'bool')],
-        "get_order_book": [('limit', 'int')],
-        "get_owner_history": [('account', 'str')],
-        "get_potential_signatures": [('signed_transaction', 'object')],
-        "get_recent_categories": [('after', 'str'), ('limit', 'int')],  # deprecated?
-        "get_recovery_request": [('account', 'str')],
-        "get_replies_by_last_update": [('account', 'str'), ('start_permlink', 'str'), ('limit', 'int')],
-        "get_required_signatures": [('signed_transaction', 'object'), ('available_keys', 'list')],
-        "get_reward_fund": [('fund_name', 'str')],  # ?
-        "get_savings_withdraw_from": [('account', 'str')],
-        "get_savings_withdraw_to": [('account', 'str')],
-        "get_state": [('path', 'str')],
-        "get_tags_used_by_author": [('account', 'str')],
-        "get_transaction": [('transaction_id', 'str')],
-        "get_transaction_hex": [('signed_transaction', 'object')],
-        "get_trending_categories": [('after', 'str'), ('limit', 'int')],
-        "get_trending_tags": [('after', 'str'), ('limit', 'int')],
-        "get_vesting_delegations": [('account', 'str'), ('from_account', 'str'), ('limit', 'int')],
-        "get_withdraw_routes": [('account', 'str'), ('withdraw_route_type', 'str')],
-        "get_witness_by_account": [('account', 'str')],
-        "get_witness_count": [],
-        "get_witness_schedule": [],
-        "get_witnesses": [],
-        "get_witnesses_by_vote": [('from_account', 'str'), ('limit', 'int')],
-        "lookup_account_names": [('account_names', 'list')],
-        "lookup_accounts": [('after', 'str'), ('limit', 'int')],
-        "lookup_witness_accounts": [('account_name', 'str'), ('limit', 'int')],
-        "set_block_applied_callback": 2,
-        "set_pending_transaction_callback": 1,
-        "set_subscribe_callback": 0,
-        "verify_account_authority": [('account', 'str'), ('keys', 'list')],
-        "verify_authority": [('signed_transaction', 'object')],
+from funcy.colls import where
+from funcy.seqs import first
+
+# todo
+# "get_expiring_vesting_delegations": [('author', 'str'), ('from_time', 'object'), ('limit', 'int')],  # ?
+# "get_reward_fund": [('fund_name', 'str')],  # ?
+
+
+api_methods = [
+    {
+        "api": "database_api",
+        "method": "set_subscribe_callback",
+        "params": [('callback', 'object'), ('clear_filter', 'object')],
+    },
+    {
+        "api": "database_api",
+        "method": "set_pending_transaction_callback",
+        "params": [('callback', 'object')],
+    },
+    {
+        "api": "database_api",
+        "method": "set_block_applied_callback",
+        "params": [('callback', 'object')],
+    },
+    {
+        "api": "database_api",
+        "method": "cancel_all_subscriptions",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_trending_tags",
+        "params": [('after_tag', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_tags_used_by_author",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_trending",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_trending30",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_created",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_active",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_cashout",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_payout",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_votes",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_children",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_hot",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_feed",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_blog",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_comments",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_promoted",
+        "params": [('discussion_query', 'dict')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_block_header",
+        "params": [('block_num', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_block",
+        "params": [('block_num', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_ops_in_block",
+        "params": [('block_num', 'int'), ('virtual_only', 'bool')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_state",
+        "params": [('path', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_trending_categories",
+        "params": [('after', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_best_categories",
+        "params": [('after', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_active_categories",
+        "params": [('after', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_recent_categories",
+        "params": [('after', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_config",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_dynamic_global_properties",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_chain_properties",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_feed_history",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_current_median_history_price",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_witness_schedule",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_hardfork_version",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_next_scheduled_hardfork",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_accounts",
+        "params": [('account_names', 'list')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_account_references",
+        "params": [('account_id', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "lookup_account_names",
+        "params": [('account_names', 'list')],
+    },
+    {
+        "api": "database_api",
+        "method": "lookup_accounts",
+        "params": [('after', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_account_count",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_conversion_requests",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_account_history",
+        "params": [('account', 'str'), ('index_from', 'int'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_owner_history",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_recovery_request",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_escrow",
+        "params": [('from_account', 'str'), ('escrow_id', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_withdraw_routes",
+        "params": [('account', 'str'), ('withdraw_route_type', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_account_bandwidth",
+        "params": [('account', 'str'), ('bandwidth_type', 'object')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_savings_withdraw_from",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_savings_withdraw_to",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_order_book",
+        "params": [('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_open_orders",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_liquidity_queue",
+        "params": [('start_account', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_transaction_hex",
+        "params": [('signed_transaction', 'object')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_transaction",
+        "params": [('transaction_id', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_required_signatures",
+        "params": [('signed_transaction', 'object'), ('available_keys', 'list')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_potential_signatures",
+        "params": [('signed_transaction', 'object')],
+    },
+    {
+        "api": "database_api",
+        "method": "verify_authority",
+        "params": [('signed_transaction', 'object')],
+    },
+    {
+        "api": "database_api",
+        "method": "verify_account_authority",
+        "params": [('account', 'str'), ('keys', 'list')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_active_votes",
+        "params": [('author', 'str'), ('permlink', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_account_votes",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_content",
+        "params": [('author', 'str'), ('permlink', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_content_replies",
+        "params": [('author', 'str'), ('permlink', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_discussions_by_author_before_date",
+        "params": [('author', 'str'), ('start_permlink', 'str'), ('before_date', 'object'), ('limit', 'int')],
+
+    },
+    {
+        "api": "database_api",
+        "method": "get_replies_by_last_update",
+        "params": [('account', 'str'), ('start_permlink', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_witnesses",
+        "params": [('witness_ids', 'list')]
+    },
+    {
+        "api": "database_api",
+        "method": "get_witness_by_account",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_witnesses_by_vote",
+        "params": [('from_account', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "lookup_witness_accounts",
+        "params": [('from_account', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "database_api",
+        "method": "get_witness_count",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_active_witnesses",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_miner_queue",
+        "params": [],
+    },
+    {
+        "api": "database_api",
+        "method": "get_vesting_delegations",
+        "params": [('account', 'str'), ('from_account', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "login_api",
+        "method": "login",
+        "params": [('username', 'str'), ('password', 'str')],
+    },
+    {
+        "api": "login_api",
+        "method": "get_api_by_name",
+        "params": [('api_name', 'str')],
+    },
+    {
+        "api": "login_api",
+        "method": "get_version",
+        "params": [],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_followers",
+        "params": [('account', 'str'), ('start_follower', 'str'), ('follow_type', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_following",
+        "params": [('account', 'str'), ('start_follower', 'str'), ('follow_type', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_follow_count",
+        "params": [('account', 'str')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_feed_entries",
+        "params": [('account', 'str'), ('entry_id', 'int'), ('limit', 'int')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_feed",
+        "params": [('account', 'str'), ('entry_id', 'int'), ('limit', 'int')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_blog_entries",
+        "params": [('account', 'str'), ('entry_id', 'int'), ('limit', 'int')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_blog",
+        "params": [('account', 'str'), ('entry_id', 'int'), ('limit', 'int')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_account_reputations",
+        "params": [('account', 'str'), ('limit', 'int')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_reblogged_by",
+        "params": [('author', 'str'), ('permlink', 'str')],
+    },
+    {
+        "api": "follow_api",
+        "method": "get_blog_authors",
+        "params": [('blog_account', 'str')],
+    },
+    {
+        "api": "network_broadcast_api",
+        "method": "broadcast_transaction",
+        "params": [('signed_transaction', 'object')],
+    },
+    {
+        "api": "network_broadcast_api",
+        "method": "broadcast_transaction_with_callback",
+        "params": [('callback', 'object'), ('signed_transaction', 'object')]
+    },
+    {
+        "api": "network_broadcast_api",
+        "method": "broadcast_transaction_synchronous",
+        "params": [('signed_transaction', 'object')],
+    },
+    {
+        "api": "network_broadcast_api",
+        "method": "broadcast_block",
+        "params": [('block', 'object')],
+    },
+    {
+        "api": "network_broadcast_api",
+        "method": "set_max_block_age",
+        "params": [('max_block_age', 'int')],
     }
-}
+]
 
 method_template = """
 def {method_name}(self{method_arguments}):
-    return self.exec('{method_name}'{call_arguments})
+    return self.exec('{method_name}'{call_arguments}, api='{api}')
 
 """
 
 
 def steemd_codegen():
     """ Generates Python methods from steemd JSON API spec. Prints to stdout. """
-    for method_name, method_signature in api_methods['api'].items():
+    for endpoint in api_methods:
         method_arg_mapper = partial(map, lambda x: ', %s: %s' % (x[0], x[1]))
         call_arg_mapper = partial(map, lambda x: ', %s' % x[0])
 
-        # todo: remove this
-        if type(method_signature) is int:
+        # skip unspecified calls
+        if endpoint['params'] == 0:
             continue
 
         # generate method code
         fn = method_template.format(
-            method_name=method_name,
-            method_arguments=''.join(method_arg_mapper(method_signature)),
-            call_arguments=''.join(call_arg_mapper(method_signature)),
+            method_name=endpoint['method'],
+            method_arguments=''.join(method_arg_mapper(endpoint['params'])),
+            call_arguments=''.join(call_arg_mapper(endpoint['params'])),
+            api=endpoint['api']
         )
         sys.stdout.write(fn)
+
+
+def find_api(method_name):
+    """ Given a method name, find its API. """
+    endpoint = first(where(api_methods, method=method_name))
+    if endpoint:
+        return endpoint.get('api')
 
 
 if __name__ == '__main__':
