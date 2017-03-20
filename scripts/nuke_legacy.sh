b5111a9ff4315532114d9eb68f825604da3d17f4
furion <_@furion.me>
2017-03-20 01:05:44 +0100

    [scripts] avoid namespace collision by uninstalling legacy apps

---
+++ scripts/nuke_legacy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pip uninstall piston-steem
+pip uninstall python-steem
+pip uninstall steem-python
+pip uninstall steem-piston
+pip uninstall steem
+pip uninstall graphenelib
+pip uninstall piston-cli
