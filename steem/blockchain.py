85d94a2fa57bc671a28c471e5ed971c95915ed3c
Gareth Nelson <gareth@garethnelson.com>
2017-08-21 17:11:59 +0100

    Steemcat/steemtail (#38)
    * Implement steemcat and reliable_stream
    
    * This is more properly "steemtail" as opposed to steemcat
    
    * make steemtail behave more like *nix tail
    
    * Tweak the sleep interval - sleep for shorter periods of time if we've output a lot of data

--- steem/blockchain.py
+++ steem/blockchain.py
@@ -4,9 +4,11 @@ import time
 import warnings
 from typing import Union
 
-from .instance import shared_steemd_instance
+from .instance import shared_steemd_instance,stm
 from .utils import parse_time
 
+import logging
+logger = logging.getLogger(__name__)
 
 class Blockchain(object):
     """ Access the blockchain and read data from it.
@@ -85,6 +87,83 @@ class Blockchain(object):
             # next round
             start_block = head_block + 1
             time.sleep(block_interval)
+
+
+    def reliable_stream(self, start_block=None, block_interval=None, update_interval=False, batch_operations=False, full_blocks=False, timeout=None, **kwargs):
+        """ A version of stream_from() intended for use in services that NEED reliable (nonstop) streaming
+
+        By default, works same as stream_from() but will also keep trying until getting a response from steemd, allowing catching up after server downtime.
+
+        Warnings:
+            To ensure reliability, this method does some weird none-standard things with the steemd client
+
+        Args:
+            start_block (int): Block to start with. If not provided, current (head) block is used.
+            block_interval (int): Time between block generations. If not provided, will attempt to query steemd for this value
+            batch_operations (bool): (Defaults to False) Rather than yielding operations one by one, 
+                yield a list of all operations for each block.
+            full_blocks (bool): (Defaults to False) Rather than yielding operations, return raw, unedited blocks as 
+                provided by steemd. This mode will NOT include virtual operations.
+            timeout (int): Time to wait on response from steemd before assuming timeout and retrying queries. If not provided, this will default to block_interval/4
+                for all queries except get_block_interval() - where it will default to 2 seconds for initial setup
+        """
+        def get_reliable_client(_timeout):
+            # we want to fail fast and try the next node quickly
+            return stm.steemd.Steemd(nodes=self.steem.nodes,retries=1,timeout=_timeout,re_raise=True)
+        def reliable_query(_client,_method,_api,*_args):
+            # this will ALWAYS eventually return, at all costs
+            retval = None
+            while retval is None:
+               try:               
+                  retval = _client.exec(_method,*_args,api=_api)
+               except Exception as e:
+                  logger.info('Failed to get response', extra=dict(exc=e,response=retval,api_name=_api,api_method=_method,api_args=_args))
+                  retval = None
+               if retval is None: time.sleep(1)
+            return retval
+
+        def get_reliable_block_interval(_client):
+            return reliable_query(_client,'get_config','database_api').get('STEEMIT_BLOCK_INTERVAL')
+
+        def get_reliable_current_block(_client):
+            return reliable_query(_client,'get_dynamic_global_properties','database_api').get(self.mode)
+
+        def get_reliable_blockdata(_client,_block_num):
+            return reliable_query(_client,'get_block', 'database_api', block_num)
+
+        def get_reliable_ops_in_block(_client,_block_num):
+            return reliable_query(_client,'get_ops_in_block','database_api',block_num,False)
+
+        if timeout is None:
+           if block_interval is None:
+              _reliable_client = get_reliable_client(2)
+              block_interval   = get_reliable_block_interval(_reliable_client)
+           else:
+              timeout          = block_interval/4
+              _reliable_client = get_reliable_client(timeout)
+        else:
+           _reliable_client = get_reliable_client(timeout)
+        if block_interval is None:
+           block_interval = get_reliable_block_interval(_reliable_client)
+        if start_block is None:
+           start_block = get_reliable_current_block(_reliable_client)
+
+        while True:
+           sleep_interval = block_interval/4
+           head_block = get_reliable_current_block(_reliable_client)
+
+           for block_num in range(start_block,head_block+1):
+               if full_blocks:
+                  yield get_reliable_current_block(_reliable_client,head_block)
+               elif batch_operations:
+                  yield get_reliable_ops_in_block(_reliable_client,head_block)
+               else:
+                  yield from get_reliable_ops_in_block(_reliable_client,head_block)
+               sleep_interval = sleep_interval/2
+
+           time.sleep(sleep_interval)
+           start_block = head_block + 1
+
 
     def stream(self, filter_by: Union[str, list] = list(), *args, **kwargs):
         """ Yield a stream of operations, starting with current head block.
