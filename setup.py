d089760004bbd55ffd68878454e46cb6c433f188
John White <jw@cyonics-MBPro.local>
2018-01-24 17:18:37 -0600

    Added basic setup files.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name='steem',
+    version='0.18.103',
+    description='Official Python Steem Library',
+    # long_description = file: README.rst
+    keywords=['steem', 'steemit', 'cryptocurrency', 'blockchain'],
+    license='MIT',
+    classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Development Status :: 4 - Beta']
+)
