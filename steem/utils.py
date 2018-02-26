5efd307e037a4544e027aa731d9fad56e8b79ccc
John White <john@steemit.com>
2018-02-26 11:20:11 -0600

    Added documentation to compat methods, and renamed them to have compat in the name for comprehension.

--- steem/utils.py
+++ steem/utils.py
@@ -366,6 +366,14 @@ def is_valid_account_name(name):
 
 
 def compose_dictionary(dictionary, **kwargs):
+    """
+    This method allows us the one line dictionary composition that is offered by the ** dictionary unpacking
+    available in 3.6.
+
+    :param dictionary: the dictionary to add the kwargs elements to.
+    :param kwargs: a set of key/value pairs to add to `dictionary`.
+    :return: the composed dictionary.
+    """
     for pairs in kwargs.items():
         dictionary[pairs[0]] = pairs[1]
 
@@ -373,6 +381,39 @@ def compose_dictionary(dictionary, **kwargs):
 
 
 def future_bytes(item, encoding=None):
+    """
+    This method is required because Python 2.7 `bytes` is simply an alias for `str`. Without this method,
+    code execution would look something like:
+
+    class clazz(object):
+
+        def __bytes__(self):
+            return bytes(5)
+
+
+    Python 2.7:
+
+    c = clazz()
+    bytes(c)
+    >>'<__main__.clazz object at 0x105171a90>'
+
+    In this example, when `bytes(c)` is invoked, the interpreter then calls `str(c)`, and prints the above string.
+    the method `__bytes__` is never invoked.
+
+    Python 3.6:
+    c = clazz()
+    bytes(c)
+    >>b'\x00\x00\x00\x00\x00'
+
+    This is the expected and necessary behavior across both platforms.
+
+    w/ future_bytes method, we will ensure that the correct bytes method is always invoked, avoiding the `str` alias in
+    2.7.
+
+    :param item: this is the object who's bytes method needs to be invoked
+    :param encoding: optional encoding parameter to handle the Python 3.6 two argument 'bytes' method.
+    :return: a bytes object that functions the same across 3.6 and 2.7
+    """
     if hasattr(item, '__bytes__'):
         return item.__bytes__()
     else:
@@ -383,6 +424,15 @@ def future_bytes(item, encoding=None):
 
 
 def to_chr(item):
+    """
+    This is necessary to maintain compatibility across Python 2.7 and 3.6.
+    In 3.6, 'chr' handles any unicode character, whereas in 2.7, `chr` only handles
+    ASCII characters. Thankfully, the Python 2.7 method `unichr` provides the same
+    functionality as 3.6 `chr`.
+
+    :param item: a length 1 string who's `chr` method needs to be invoked
+    :return: the unichr code point of the single character string, item
+    """
     if sys.version >= '3.0':
         return chr(item)
     else:
