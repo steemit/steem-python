df61b5d13e3987e3f228348278cfa4cb50393079
John White <john@steemit.com>
2018-03-09 16:37:13 -0600

    Removed long_description from setup.py for right now.

--- setup.py
+++ setup.py
@@ -61,8 +61,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+# with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+#     long_description = '\n' + f.read()
 
 
 class UploadCommand(Command):
@@ -104,7 +104,7 @@ setup(
     version='0.18.3',
     description=DESCRIPTION,
     keywords=['steem', 'steemit', 'cryptocurrency', 'blockchain'],
-    long_description=long_description,
+    # long_description=long_description,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
