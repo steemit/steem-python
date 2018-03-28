e71c5448fa1153c1d62bab41b9be0c85996b8657
roadscape <roadscape@users.noreply.github.com>
2018-03-28 12:16:14 -0500

    pull assert out of catch

--- tests/steem/test_broadcast.py
+++ tests/steem/test_broadcast.py
@@ -7,12 +7,15 @@ def test_claim_reward():
     wif = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
     c = Commit(steem=Steem(keys=[wif]))
 
+    rpc_error = None
     try:
         c.claim_reward_balance(account='test')
     except RPCError as e:
-        assert 'missing required posting authority' in str(e)
+        rpc_error = str(e)
     else:
         raise Exception('expected RPCError')
+
+    assert 'missing required posting authority' in rpc_error
 
 
 def test_witness_update():
@@ -24,9 +27,12 @@ def test_witness_update():
         'maximum_block_size': 65536,
         'sbd_interest_rate': 0}
 
+    rpc_error = None
     try:
         c.witness_update(signing_key=key, account='test', props=props, url='')
     except RPCError as e:
-        assert 'missing required active authority' in str(e)
+        rpc_error = str(e)
     else:
         raise Exception('expected RPCError')
+
+    assert 'missing required active authority' in rpc_error
