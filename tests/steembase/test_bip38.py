54097009fcd879dc9215409304d4d55729251256
John G G <jg@steemit.com>
2018-05-08 12:17:18 -0400

    tweek ENV var handling

--- tests/steembase/test_bip38.py
+++ tests/steembase/test_bip38.py
@@ -5,20 +5,22 @@ from steembase.account import PrivateKey
 
 class Testcases(unittest.TestCase):
     def test_encrypt(self):
-        from steembase.bip38 import encrypt
+        if 'SCRYPT_MODULE' in os.environ:
+            del os.environ['SCRYPT_MODULE']
+        import steembase.bip38
         self.assertEqual([
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5HqUkGuo62BfcJU5vNhTXKJRXuUi9QSE6jp8C3uBJ2BVHtB8WSd"),
                     "TestingOneTwoThree"), "encwif"),
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR"),
                     "TestingOneTwoThree"), "encwif"),
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5HtasZ6ofTHP6HCwTqTkLDuLQisYPah7aUnSKfC7h4hMUVw2gi5"),
                     "Satoshi"), "encwif")
@@ -29,16 +31,18 @@ class Testcases(unittest.TestCase):
         ])
 
     def test_decrypt(self):
-        from steembase.bip38 import decrypt
+        if 'SCRYPT_MODULE' in os.environ:
+            del os.environ['SCRYPT_MODULE']
+        import steembase.bip38
         self.assertEqual([
             format(
-                decrypt("6PRN5mjUTtud6fUXbJXezfn6oABoSr6GSLjMbrGXRZxSUcxTh"
+                steembase.bip38.decrypt("6PRN5mjUTtud6fUXbJXezfn6oABoSr6GSLjMbrGXRZxSUcxTh"
                         "xsUW8epQi", "TestingOneTwoThree"), "wif"),
             format(
-                decrypt("6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEb"
+                steembase.bip38.decrypt("6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEb"
                         "n2Nh2ZoGg", "TestingOneTwoThree"), "wif"),
             format(
-                decrypt("6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTe"
+                steembase.bip38.decrypt("6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTe"
                         "PPX1dWByq", "Satoshi"), "wif")
         ], [
             "5HqUkGuo62BfcJU5vNhTXKJRXuUi9QSE6jp8C3uBJ2BVHtB8WSd",
@@ -48,20 +52,21 @@ class Testcases(unittest.TestCase):
 
     def test_encrypt_scrypt(self):
         os.environ['SCRYPT_MODULE'] = 'scrypt'
-        from steembase.bip38 import encrypt
+        import steembase.bip38
+        assert steembase.bip38.SCRYPT_MODULE == 'scrypt'
         self.assertEqual([
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5HqUkGuo62BfcJU5vNhTXKJRXuUi9QSE6jp8C3uBJ2BVHtB8WSd"),
                     "TestingOneTwoThree"), "encwif"),
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR"),
                     "TestingOneTwoThree"), "encwif"),
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5HtasZ6ofTHP6HCwTqTkLDuLQisYPah7aUnSKfC7h4hMUVw2gi5"),
                     "Satoshi"), "encwif")
@@ -70,44 +75,46 @@ class Testcases(unittest.TestCase):
             "6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEbn2Nh2ZoGg",
             "6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTePPX1dWByq"
         ])
-        os.environ['SCRYPT_MODULE'] = ''
+        del os.environ['SCRYPT_MODULE']
 
     def test_decrypt_scrypt(self):
         os.environ['SCRYPT_MODULE'] = 'scrypt'
-        from steembase.bip38 import decrypt
+        import steembase.bip38
+        assert steembase.bip38.SCRYPT_MODULE == 'scrypt'
         self.assertEqual([
             format(
-                decrypt("6PRN5mjUTtud6fUXbJXezfn6oABoSr6GSLjMbrGXRZxSUcxTh"
+                steembase.bip38.decrypt("6PRN5mjUTtud6fUXbJXezfn6oABoSr6GSLjMbrGXRZxSUcxTh"
                         "xsUW8epQi", "TestingOneTwoThree"), "wif"),
             format(
-                decrypt("6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEb"
+                steembase.bip38.decrypt("6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEb"
                         "n2Nh2ZoGg", "TestingOneTwoThree"), "wif"),
             format(
-                decrypt("6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTe"
+                steembase.bip38.decrypt("6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTe"
                         "PPX1dWByq", "Satoshi"), "wif")
         ], [
             "5HqUkGuo62BfcJU5vNhTXKJRXuUi9QSE6jp8C3uBJ2BVHtB8WSd",
             "5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR",
             "5HtasZ6ofTHP6HCwTqTkLDuLQisYPah7aUnSKfC7h4hMUVw2gi5"
         ])
-        os.environ['SCRYPT_MODULE'] = ''
+        del os.environ['SCRYPT_MODULE']
 
     def test_encrypt_pylibscrypt(self):
         os.environ['SCRYPT_MODULE'] = 'pylibscrypt'
-        from steembase.bip38 import encrypt
+        import steembase.bip38
+        assert steembase.bip38.SCRYPT_MODULE == 'pylibscrypt'
         self.assertEqual([
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5HqUkGuo62BfcJU5vNhTXKJRXuUi9QSE6jp8C3uBJ2BVHtB8WSd"),
                     "TestingOneTwoThree"), "encwif"),
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR"),
                     "TestingOneTwoThree"), "encwif"),
             format(
-                encrypt(
+                steembase.bip38.encrypt(
                     PrivateKey(
                         "5HtasZ6ofTHP6HCwTqTkLDuLQisYPah7aUnSKfC7h4hMUVw2gi5"),
                     "Satoshi"), "encwif")
@@ -116,27 +123,28 @@ class Testcases(unittest.TestCase):
             "6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEbn2Nh2ZoGg",
             "6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTePPX1dWByq"
         ])
-        os.environ['SCRYPT_MODULE'] = ''
+        del os.environ['SCRYPT_MODULE']
 
     def test_decrypt_pylibscrypt(self):
         os.environ['SCRYPT_MODULE'] = 'pylibscrypt'
-        from steembase.bip38 import decrypt
+        import steembase.bip38
+        assert steembase.bip38.SCRYPT_MODULE == 'pylibscrypt'
         self.assertEqual([
             format(
-                decrypt("6PRN5mjUTtud6fUXbJXezfn6oABoSr6GSLjMbrGXRZxSUcxTh"
+                steembase.bip38.decrypt("6PRN5mjUTtud6fUXbJXezfn6oABoSr6GSLjMbrGXRZxSUcxTh"
                         "xsUW8epQi", "TestingOneTwoThree"), "wif"),
             format(
-                decrypt("6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEb"
+                steembase.bip38.decrypt("6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEb"
                         "n2Nh2ZoGg", "TestingOneTwoThree"), "wif"),
             format(
-                decrypt("6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTe"
+                steembase.bip38.decrypt("6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTe"
                         "PPX1dWByq", "Satoshi"), "wif")
         ], [
             "5HqUkGuo62BfcJU5vNhTXKJRXuUi9QSE6jp8C3uBJ2BVHtB8WSd",
             "5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR",
             "5HtasZ6ofTHP6HCwTqTkLDuLQisYPah7aUnSKfC7h4hMUVw2gi5"
         ])
-        os.environ['SCRYPT_MODULE'] = ''
+        del os.environ['SCRYPT_MODULE']
 
 if __name__ == '__main__':
     unittest.main()
