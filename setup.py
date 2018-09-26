cebe98d78f00f3c624fc4a0fbb36dc3d595981e1
Donovan Walker <donovan@donovanwalker.com>
2018-09-26 15:21:21 -0500

    [#267] remove -rc1 from version

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ class UploadCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version='1.0.2-rc1',
+    version='1.0.2',
     description=DESCRIPTION,
     keywords=['steem', 'steemit', 'cryptocurrency', 'blockchain'],
     # long_description=long_description,
